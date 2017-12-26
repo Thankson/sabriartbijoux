5a577e03b74eea15c4f0b0884ac0faa508816acd
zengqingwen <790494072@qq.com>
2017-12-26 10:41:24 +0800

    sabriart bijoux

---
+++ sito/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+from sito.models import *
+from image_cropping import ImageCroppingMixin
+
+
+class MyModelAdmin(ImageCroppingMixin, admin.ModelAdmin):
+    pass
+
+class SliderAdmin(ImageCroppingMixin, admin.ModelAdmin):
+    list_display = ("image_img", "titolo", "active")
+
+class ImmaginiAdmin(ImageCroppingMixin, admin.ModelAdmin):
+    list_display = ("image_img", "titolo")
+
+def get_category(self):
+    return self.categoria.all()
+
+class PostAdmin(ImageCroppingMixin, admin.ModelAdmin):
+    #duplicate function
+    def duplicate_event(ModelAdmin, request, queryset):
+        for object in queryset:
+            object.id = None
+            object.save()
+    duplicate_event.short_description = "Duplica Record Selezionati"
+
+    list_display = ("image_img", "titolo")
+    actions = ['duplicate_event']
+
+
+
+admin.site.register(Categorie, MyModelAdmin)
+admin.site.register(Immagini, ImmaginiAdmin)
+admin.site.register(Video, MyModelAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Box, MyModelAdmin)
+admin.site.register(News, MyModelAdmin)
+admin.site.register(Slider, ImmaginiAdmin)
+admin.site.register(Page, MyModelAdmin)
+admin.site.register(TipologiaEventi, MyModelAdmin)
+
+
+
---
+++ sito/views.py
@@ -0,0 +1,142 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.views.generic import ListView, DetailView
+from django.template.loader import render_to_string
+from sito.models import *
+from django.core.mail import send_mail
+from filer.models import *
+
+from datetime import datetime
+
+# Create your views here
+
+def HomePage(request):
+    post_list = Post.objects.all().order_by('-id')
+    slider_list = Post.objects.all().order_by('pub_date')
+    news_list = News.objects.all().order_by('-pub_date')[:4]
+    box_list = Box.objects.all()
+    gioielli = Page.objects.get(id=1)
+    biografia = Page.objects.get(id=2)
+    slogan = Page.objects.get(id=3)
+    context = {'post_list':post_list,
+                'slider_list':slider_list,
+                'news_list':news_list,
+                'box_list':box_list,
+                'gioielli':gioielli,
+                'biografia':biografia,
+                'slogan':slogan}
+    return render_to_response('index.html', context, context_instance=RequestContext(request))
+
+
+
+def ProductView(request):
+    post_list = Post.objects.all().order_by('-id')
+    categorie_list = Categorie.objects.all()
+    context = {'post_list':post_list,
+                'categorie_list':categorie_list}
+    return render_to_response('prodotti.html', context, context_instance=RequestContext(request))
+
+
+def ProductFilterView(request, post_id):
+    post_list = Post.objects.filter(categoria_id = post_id).order_by('-id')
+    categorie_list = Categorie.objects.all()
+    context = {'post_list':post_list,
+                'categorie_list':categorie_list}
+    return render_to_response('prodotti.html', context, context_instance=RequestContext(request))
+
+
+def DetailView(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    filer_list = Image.objects.filter(folder_id = post.album)
+    context = {'post': post,
+    			'filer_list':filer_list}
+    return render_to_response('dettaglio.html', context, context_instance=RequestContext(request))
+
+
+def categorie(request):
+	categorie_list = Categorie.objects.all()
+	context = {'categorie_list':categorie_list}
+	return context
+
+
+def news(request):
+    tipologie_list = TipologiaEventi.objects.all()
+    news_list = News.objects.all().order_by('-pub_date')
+    context = {'tipologie_list':tipologie_list,
+                'news_list':news_list}
+    return render_to_response('news.html', context, context_instance=RequestContext(request))
+
+def newsFilter(request, post_id):
+    tipologie_list = TipologiaEventi.objects.all()
+    news_list = News.objects.filter(categoria_id=post_id).order_by('-pub_date')
+    categoria = TipologiaEventi.objects.get(id=post_id)
+    context = {'tipologie_list':tipologie_list,
+                'news_list':news_list,
+                'categoria':categoria}
+    return render_to_response('news.html', context, context_instance=RequestContext(request))
+
+
+### EMAIL - CONTACT FORM
+def contact(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            subject = 'MESSAGGIO DAL SITO SABRIART BIJOUX'
+            #message = form.cleaned_data['messaggio']
+            message = render_to_string('contact.txt', {'post': request.POST})
+            sender = form.cleaned_data['email']
+            cc_myself = False
+
+            recipients = ['info@sabriartbijoux.it']
+            if cc_myself:
+                recipients.append(sender)
+        
+            send_mail(subject, message, sender, recipients)
+            return HttpResponseRedirect('/success/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    #return render_to_response('contact.html', {'form': form,})
+    return render_to_response('contact.html', context_instance=RequestContext(request))
+
+
+
+### EMAIL - CONTACT FORM
+def shop(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            subject = 'MESSAGGIO DAL SITO SABRIART BIJOUX'
+            #message = form.cleaned_data['messaggio']
+            message = render_to_string('contact.txt', {'post': request.POST})
+            sender = form.cleaned_data['email']
+            cc_myself = False
+
+            recipients = ['info@sabriartbijoux.it']
+            if cc_myself:
+                recipients.append(sender)
+        
+            send_mail(subject, message, sender, recipients)
+            return HttpResponseRedirect('/success/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    #return render_to_response('contact.html', {'form': form,})
+    return render_to_response('shop.html', context_instance=RequestContext(request))
+
+
+
+
+def success(request):
+    return render_to_response('success.html', context_instance=RequestContext(request))
+
+
+### setting language session
+def language(request, language='it'):
+    response = HttpResponse("setting language to %s" % language)
+    response.set_cookie('lang', language)
+    request.session['lang'] = language
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
